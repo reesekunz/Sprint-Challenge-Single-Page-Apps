522d4d6959c64dde4885abf93bed641e5a749d0f
Reese Kunz <reesekunz21@gmail.com>
2019-07-26 14:23:03 -0700

    styled episode cards

--- src/components/EpisodeCard.js
+++ src/components/EpisodeCard.js
@@ -1,14 +1,30 @@
 import React from "react";
   // passing props of what is being returned from api data that you want to show for location section
+import styled from "styled-components";
+
+const StyledCards = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid black;
+  background-color: lightblue;
+  width: 40%;
+  padding: 2%;
+  margin: 3%;
+`;
+
 
 function EpisodeCard ({ name, air_date, episode }) {
 
   return (
+    <StyledCards>
     <div className="character-card">
-      <h3 className="Name">Name: {name}</h3>
-      <h3 className="Type">Type: {air_date}</h3>
+      <h3 className="Name">{name}</h3>
+      <h3 className="Type">Air-Date: {air_date}</h3>
       <h3 className="Dimension">Dimension: {episode}</h3>
-  </div> )
+  </div>
+  </StyledCards> )
 }
 
 export default EpisodeCard;
