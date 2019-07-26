8e7ad7292a6a597b73b4383a69435bc7ee5b5cec
Reese Kunz <reesekunz21@gmail.com>
2019-07-26 08:55:04 -0700

    added correct character endpoint

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,28 +1,42 @@
 import React, { useEffect, useState } from 'react';
+import axios from "axios";
+
 
 function CharacterList(props) {
   // TODO: Add useState to track data from useEffect
-  const [character, setCharacter] = useState();
+  const [characters, setCharacters] = useState({});
 
   useEffect(() => {
     // TODO: Add AJAX/API Request here - must run in `useEffect`
     axios
-      .get(`https://rickandmortyapi.com/documentation/#get-all-characters`)
+      .get(`https://rickandmortyapi.com/api/character/`)
+      // handles success 
       .then(response => {
-        setCharacter('component mounted', response.results);
+        setCharacters(response.results);
       })
+      // handles failure 
       .catch(error => {
-        console.error('no bueno', error);
+        console.log('no bueno', error);
       });
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, [])
+  }, [setCharacters])
 
-  return <section className='character-list grid-view'>
 
-      <h2>TODO: `array.map()` over your state here!</h2>
+console.log(response.results)
+  return (
+  <section className='character-list grid-view'>
+{/* TODO: `array.map()` over your state here! */}
+     {/* characters.map(character => {
+
+
+
+     } */}
+     
+     
+     
     </section>
+  )
 
-}
-
+    }
 
 export default CharacterList;
