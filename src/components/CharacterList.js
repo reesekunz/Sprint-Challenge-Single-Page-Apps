65ef9c505e3a6ad26ae37c5ea84b9bf4fb177143
Reese Kunz <reesekunz21@gmail.com>
2019-07-26 10:03:44 -0700

    mapping returning undefined :(

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -10,22 +10,43 @@ function CharacterList() {
     axios
       .get(`https://rickandmortyapi.com/api/character/`)
       // handles success
-      .then(response => {
-        setCharacters(response.characters);
-      })
+      .then(response => setCharacters(response.characters))
+      
       // handles failure
       .catch(error => {
         console.log("no bueno", error);
-      });
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
+        //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+      }, []);   
+    })
 
-  console.log("component mounted, data = ", characters);
-  return (
-    <section className="character-list grid-view">
-      {/* TODO: `array.map()` over your state here! */}
-    </section>
-  );
+    // Console log handles success to  make sure component mounted 
+    console.log("component did mount, characters data = ", characters);
+    
+    // console.log(results.name);
+
+  
+    if (characters.results === undefined) {
+    return <div />;
+  } else {
+    return (
+      <div>
+        {characters.map(character => {
+          return (
+         <div> 
+              {/* <h3 className="id">Id: {results.id}</h3>
+              <h3 className="name">Name: {results.name}</h3>
+              <h3 className="status">Status: {results.status}</h3>
+              <h3 className="species">Species: {results.species}</h3>
+              <h3 className="type">Type: {results.type}</h3>
+              <h3 className="gender">Gender: {results.gender}</h3>
+              <h3 className="origin">Origin: {results.origin}</h3> */}
+
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default CharacterList;
