fbb81cee327e7a741d58c521465f59abdbe8c1ff
Reese Kunz <reesekunz21@gmail.com>
2019-07-26 13:09:28 -0700

    wrapped root component

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import 'semantic-ui-css/semantic.min.css'
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<Router>
+ReactDOM.render(
+  // wrap your app with the router - renames component to router inside file 
+  <Router>
   <App />
-</Router>, document.getElementById('root'));
+  </Router>,
+  document.getElementById('root'));
