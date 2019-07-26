078808477885db25805a3b7b3d500892ea75c13d
Reese Kunz <reesekunz21@gmail.com>
2019-07-26 10:23:24 -0700

    added prop paramters

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,18 +1,20 @@
 import React from "react";
 
-function CharacterCard(props) {
+function CharacterCard({
+  id, name, status, species, type, gender, origin, name, url, location, image, episode, created }
+}
+ {
   console.log("props", props);
   console.log("character card name props", props.data.results);
 
   return (
     <div className="CharacterCard">
-      <h3 className="id">Id: {results.id}</h3>
-      <h3 className="name">Name: {results.name}</h3>
-      <h3 className="status">Status: {results.status}</h3>
-      <h3 className="species">Species: {results.species}</h3>
-      <h3 className="type">Type: {results.type}</h3>
-      <h3 className="gender">Gender: {results.gender}</h3>
-      <h3 className="origin">Origin: {results.origin}</h3>
+      <h3 className="name">Name: {props.results.name}</h3>
+      <h3 className="status">Status: {props.results.status}</h3>
+      <h3 className="species">Species: {props.results.species}</h3>
+      <h3 className="type">Type: {props.results.type}</h3>
+      <h3 className="gender">Gender: {props.results.gender}</h3>
+      <h3 className="origin">Origin: {props.results.origin}</h3>
     </div>
   );
 }
