17e4d23b893568eca90ed4949352684ca0a1d5d4
Reese Kunz <reesekunz21@gmail.com>
2019-07-26 10:17:02 -0700

    added props for character card

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from "react";
 
-function CharacterCard ({ /* add props */ }) {
-  return (<span>todo: location</span>)
+function CharacterCard(props) {
+  console.log("props", props);
+  console.log("character card name props", props.data.results);
+
+  return (
+    <div className="CharacterCard">
+      <h3 className="id">Id: {results.id}</h3>
+      <h3 className="name">Name: {results.name}</h3>
+      <h3 className="status">Status: {results.status}</h3>
+      <h3 className="species">Species: {results.species}</h3>
+      <h3 className="type">Type: {results.type}</h3>
+      <h3 className="gender">Gender: {results.gender}</h3>
+      <h3 className="origin">Origin: {results.origin}</h3>
+    </div>
+  );
 }
-
 
 export default CharacterCard;
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,48 +1,35 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
+import CharacterCard from "./CharacterCard";
+
+
 function CharacterList() {
   // TODO: Add useState to track data from useEffect
-  const [characters, setCharacters] = useState({});
+  const [data, setData] = useState({});
 
   useEffect(() => {
     // TODO: Add AJAX/API Request here - must run in `useEffect`
     axios
       .get(`https://rickandmortyapi.com/api/character/`)
       // handles success
-      .then(response => setCharacters(response.characters))
-      
-      // handles failure
-      .catch(error => {
-        console.log("no bueno", error);
-        //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-      }, []);   
-    })
+      .then(resolved => setData(resolved.data))
 
-    // Console log handles success to  make sure component mounted 
-    console.log("component did mount, characters data = ", characters);
-    
-    // console.log(results.name);
+      // Handles failure
+      .catch(error => console.log("uh oh", error));
+    // Add empty dependency array to avoid infinite API requests
+  }, []);
 
-  
-    if (characters.results === undefined) {
+  // Console log handles success to  make sure component mounted
+  console.log("component did mount, data = ", data);
+
+  if (data.results === undefined) {
     return <div />;
   } else {
     return (
       <div>
-        {characters.map(character => {
-          return (
-         <div> 
-              {/* <h3 className="id">Id: {results.id}</h3>
-              <h3 className="name">Name: {results.name}</h3>
-              <h3 className="status">Status: {results.status}</h3>
-              <h3 className="species">Species: {results.species}</h3>
-              <h3 className="type">Type: {results.type}</h3>
-              <h3 className="gender">Gender: {results.gender}</h3>
-              <h3 className="origin">Origin: {results.origin}</h3> */}
-
-            </div>
-          );
+        {data.results.map((item, index) => {
+          return <CharacterCard key={index} data={item} />;
         })}
       </div>
     );
