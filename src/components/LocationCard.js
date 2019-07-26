0347dfd16686481a4960e2ed02c1ce7007691287
Reese Kunz <reesekunz21@gmail.com>
2019-07-26 14:24:49 -0700

    styled location cards

--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -1,14 +1,29 @@
 import React from "react";
   // passing props of what is being returned from api data that you want to show for location section
+  import styled from "styled-components";
+
+  const StyledCards = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid black;
+  background-color: lightblue;
+  width: 40%;
+  padding: 2%;
+  margin: 3%;
+`;
 
 function LocationCard ({ name, type, dimension, residents }) {
 
   return (
+    <StyledCards>
     <div className="character-card">
       <h3 className="Name">Name: {name}</h3>
       <h3 className="Type">Type: {type}</h3>
       <h3 className="Dimension">Dimension: {dimension}</h3>
-  </div> )
+  </div>
+  </StyledCards> )
 }
 
 export default LocationCard;
